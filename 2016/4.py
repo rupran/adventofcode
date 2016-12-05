67072a6cd58ef15991564d84b630a54898efe69a
Andreas Ziegler <andreas.ziegler@fau.de>
2016-12-05 13:55:39 +0100

    Day 4: Port to new structure

--- 2016/4.py
+++ 2016/4.py
@@ -17,33 +17,46 @@ def decrypt(line, factor):
 
     return res
 
-sum_of_valid_ids = 0
-
-for line in lib.get_input(4):
+def get_parts(line):
     # Get characters, sector id and checksum from line
     match_id = re.match("^([a-z-]+)([0-9]+)\[([a-z]+)\]$", line)
+
+    name = match_id.group(1)
     sector_id = int(match_id.group(2))
     checksum = match_id.group(3)
-    chars_in_name = [x for x in match_id.group(1) if x != '-']
 
-    # Count characters
-    counts = collections.defaultdict(int)
-    for letter in chars_in_name:
-        counts[letter] += 1
+    return name, sector_id, checksum
 
-    # Sort by number of occurrences decreasing, then by letter increasing
-    reverse_key = lambda a: (-a[1], a[0])
-    sorted_counts = sorted(counts.items(), key=reverse_key)
+def part_one(line_gen):
+    sum_of_valid_ids = 0
 
-    # Compare five most common characters to checksum and add to sum
-    five_most_common = "".join(x[0] for x in sorted_counts[:5])
-    if five_most_common == checksum:
-        sum_of_valid_ids += sector_id
+    for line in line_gen:
+        name, sector_id, checksum = get_parts(line)
+        chars_in_name = [x for x in name if not x == '-']
 
+        # Count characters
+        counts = collections.defaultdict(int)
+        for letter in chars_in_name:
+            counts[letter] += 1
+
+        # Sort by number of occurrences decreasing, then by letter increasing
+        reverse_key = lambda a: (-a[1], a[0])
+        sorted_counts = sorted(counts.items(), key=reverse_key)
+
+        # Compare five most common characters to checksum and add to sum
+        five_most_common = "".join(x[0] for x in sorted_counts[:5])
+        if five_most_common == checksum:
+            sum_of_valid_ids += sector_id
+
+    return sum_of_valid_ids
+
+def part_two(line_gen):
     # Part B: decrypt line by shifting keys
-    decrypted = decrypt(match_id.group(1), sector_id)
-    if "northpole object storage" in decrypted:
-        storage_id = sector_id
+    for line in line_gen:
+        name, sector_id, _ = get_parts(line)
+        decrypted = decrypt(name, sector_id)
+        if "northpole object storage" in decrypted:
+            return sector_id
 
-print("A: " + str(sum_of_valid_ids))
-print("B: " + str(storage_id))
+print("A: " + str(part_one(lib.get_input(4))))
+print("B: " + str(part_two(lib.get_input(4))))
