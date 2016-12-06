b2cc1162cb0fca220213029afe6588184e0412f2
Andreas Ziegler <andreas.ziegler@fau.de>
2016-12-06 08:53:13 +0100

    Only download file if it doesn't exist and possibly use Last-modified header

--- 2016/get.sh
+++ 2016/get.sh
@@ -5,6 +5,11 @@ YEAR=2016
 
 # session.cookie must contain the cookie for adventofcode.com in Netscape format
 
+if [ $# -ne 1 ]; then
+    echo "Error: provide day as the only parameter!";
+    exit 1;
+fi
+
 day=$1
 
 if [ -f $day.py ]; 
@@ -16,5 +21,7 @@ else
     sed -i s/CURRENT_DAY/$day/g $day.py;
     chmod u+x $day.py;
     touch inputs/sample_$day;
-    wget --load-cookies=session.cookie http://adventofcode.com/$YEAR/day/$day/input -O inputs/input_$day;
+    if ! [ -f inputs/input_$day ]; then
+        wget --load-cookies=session.cookie -N https://adventofcode.com/$YEAR/day/$day/input > inputs/input_$day;
+    fi
 fi
