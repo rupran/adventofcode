ea8159c3f05947b1df9865eb4f5122a1ee24c10c
Andreas Ziegler <andreas.ziegler@fau.de>
2016-12-09 10:46:15 +0100

    AoC 2016, day 9: Part 2 + rewrite of Part 1 to not actually build the string

--- 2016/9.py
+++ 2016/9.py
@@ -2,52 +2,65 @@
 
 import lib.common as lib
 
-def part_one(line_gen):
-    line = next(line_gen)
-    total_str = ""
-    line_len = len(line)
-    index = 0
-    while index < line_len:
-        next_count = 0
+def get_index_count_repeatlen(line, index):
+    next_count = 0
+    repeat_len = 0
+    
+    index += 1
+    char = line[index]
+    while char != "x":
+        next_count *= 10
+        next_count += int(char)
+        index += 1
         char = line[index]
-        next_string = ""
+    index += 1
+    char = line[index]
+    while char != ")":
+        repeat_len *= 10
+        repeat_len += int(char)
+        index += 1
+        char = line[index]
+    index += 1
+
+    return index, next_count, repeat_len
+
+def eval_pattern_length_one(line, offset, length):
+    index = offset
+    cur_len = 0
+    while index < offset + length:
+        char = line[index]
         if char != "(" and char != ")":
-            next_string += char
+            cur_len += 1
             index += 1
         else:
-            if char == "(":
-                index += 1
-                char = line[index]
-                repeat_pattern = ""
-                repeat_len = 0
-                while char != "x":
-                    next_count *= 10
-                    next_count += int(char)
-                    index += 1
-                    char = line[index]
-                index += 1
-                char = line[index]
-                while char != ")":
-                    repeat_len *= 10
-                    repeat_len += int(char)
-                    index += 1
-                    char = line[index]
-                index += 1
-                for _ in range(next_count):
-                    char = line[index]
-                    repeat_pattern += char
-                    index += 1
+            index, next_count, repeat_len = get_index_count_repeatlen(line, index)
+            cur_len += (repeat_len * next_count)
+            index += next_count
 
-                next_string = "".join([repeat_pattern] * repeat_len)
+    return cur_len
 
-        total_str += next_string
+def eval_pattern_length_two(line, offset, length):
+    index = offset
+    cur_len = 0
+    while index < offset + length:
+        char = line[index]
+        if char != "(" and char != ")":
+            cur_len += 1
+            index += 1
+        else:
+            index, next_count, repeat_len = get_index_count_repeatlen(line, index)
+            cur_len += eval_pattern_length_two(line, index, next_count) * repeat_len
+            index += next_count
 
-    return len(total_str)
+    return cur_len
+    
+def part_one(line_gen):
+    line = next(line_gen)
+    return eval_pattern_length_one(line, 0, len(line))
 
 def part_two(line_gen):
-    # line = next(line_gen) # one line input
-    # for line in line_gen: # multi line input
-    pass
+    line = next(line_gen) # one line input
+    return eval_pattern_length_two(line, 0, len(line))
 
 print("A: " + str(part_one(lib.get_input(9))))
 print("B: " + str(part_two(lib.get_input(9))))
