122eedd60bc54e9114aaed40999ca20888654201
Andreas Ziegler <andreas.ziegler@fau.de>
2016-12-20 09:30:14 +0100

    AoC 2016, day 20

---
+++ 2016/20.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import lib.common as lib
+
+def merge_ranges(line_gen):
+    ranges = sorted((int(x[0]), int(x[1])) for x in 
+                        [l.split("-") for l in line_gen])
+
+    merged_ranges = [ranges[0]]
+    cur_ind = 1
+    while cur_ind < len(ranges):
+        start_a, end_a = merged_ranges[-1]
+        start_b, end_b = ranges[cur_ind]
+        # Merge overlapping and adjacent, only keep merged pair
+        # overlapping:  end_a >= start_b
+        # adjacent: end_a + 1 == start_b
+        if end_a + 1 >= start_b:
+            merged_range = (start_a, max(end_a, end_b))
+            merged_ranges.pop()
+            merged_ranges.append(merged_range)
+        # No merge possible, add this pair
+        else:
+            merged_ranges.append(ranges[cur_ind])
+        cur_ind += 1
+
+    return merged_ranges
+
+def part_one(line_gen):
+    merged_ranges = merge_ranges(line_gen)
+    return merged_ranges[0][1] + 1
+
+def part_two(line_gen, max_ip):
+    merged_ranges = merge_ranges(line_gen)
+    # Add fake range which blocks ports > max_ip
+    merged_ranges.append((max_ip + 1, None))
+
+    allowed = 0
+    cur_ind = 0
+    while cur_ind < len(merged_ranges) - 1:
+        allowed += merged_ranges[cur_ind + 1][0] - merged_ranges[cur_ind][1] - 1
+        cur_ind += 1
+
+    return allowed
+
+print("A: " + str(part_one(lib.get_input(20))))
+print("B: " + str(part_two(lib.get_input(20), 4294967295)))
