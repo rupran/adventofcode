2398a392fb4ee2e611a4d54906d4d5d0b0ee2861
Andreas Ziegler <andreas.ziegler@fau.de>
2017-12-04 10:00:16 +0100

    AoC 2017, day 2

---
+++ 2017/2.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import lib.common as lib
+
+def part_one(line_gen):
+    checksum = 0
+    for line in line_gen: # multi line input
+        line = [int(x) for x in line.split()]
+        checksum += (max(line) - min(line))
+    return checksum
+
+def part_two(line_gen):
+    checksum = 0
+    for line in line_gen: # multi line input
+        line = [int(x) for x in line.split()]
+        found = False
+        for idx, x in enumerate(line):
+            if found:
+                break
+            for y in line[idx + 1:]:
+                bigger = max(x, y)
+                smaller = min(x, y)
+                if bigger % smaller == 0:
+                    checksum += (bigger/smaller)
+                    found = True
+    return checksum
+
+
+print("A: " + str(part_one(lib.get_input(2))))
+print("B: " + str(part_two(lib.get_input(2))))
