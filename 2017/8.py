691d8d7c47a9a0c055422f5da6f06c60fb102e06
Andreas Ziegler <andreas.ziegler@fau.de>
2017-12-08 10:00:40 +0100

    AoC 2017, day 8: simplify

--- 2017/8.py
+++ 2017/8.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import collections
 import re
 
 import lib.common as lib
@@ -8,34 +9,18 @@ class Instr:
 
     def __init__(self, target, op, val, condition):
         self.target = target
-        if op == 'inc':
-            op = '+'
-        else:
-            op = '-'
+        op = '+' if op == 'inc' else '-'
         self.instruction = 'registers[\'{}\'] {}= {}'.format(target, op, val)
-        self.cond_target = re.match(r'^(\w+)', condition).group(0)
         self.condition = re.sub(r'^(\w+)', 'registers[\'\g<0>\']', condition)
 
 def simulate(line_gen, part_two=False):
     program = []
-    registers = {}
-    # line = next(line_gen) # one line input
+    registers = collections.defaultdict(int)
+    max_val = 0
+
     for line in line_gen:
         match = re.match("^(\w+) (inc|dec) ([0-9-]+) if (.+)$", line)
-        if match:
-            program.append(Instr(*match.groups()))
-
-    ip = 0
-    max_val = 0
-    while ip < len(program):
-        cur = program[ip]
-        ip += 1
-
-        if not cur.target in registers:
-            registers[cur.target] = 0
-
-        if not cur.cond_target in registers:
-            registers[cur.cond_target] = 0
+        cur = Instr(*match.groups())
 
         if eval(cur.condition):
             exec(cur.instruction)
@@ -46,7 +31,6 @@ def simulate(line_gen, part_two=False):
         return max_val
     else:
         return max(registers.values())
-
 
 def part_one(line_gen):
     return simulate(line_gen)
