545a93e89e5e61375758f9df91fa49896071c414
Andreas Ziegler <andreas.ziegler@fau.de>
2020-12-18 17:05:14 +0100

    AoC 2020, day 18: unify logic

--- 2020/18.py
+++ 2020/18.py
@@ -15,6 +15,20 @@ def calc_flat_formula(tokens, from_idx, to_idx):
         prod *= s
     return prod
 
+def accumulate_flat_formula(tokens, from_idx, to_idx):
+    result = int(tokens[from_idx])
+    from_idx += 1
+    cur_op = None
+    while from_idx < to_idx:
+        if tokens[from_idx] in ('+', '*'):
+            cur_op = tokens[from_idx]
+        elif cur_op == '+':
+            result += int(tokens[from_idx])
+        elif cur_op == '*':
+            result *= int(tokens[from_idx])
+        from_idx += 1
+    return result
+
 def find_closing_bracket(tokens, from_idx):
     # Find the corresponding closing bracket for the bracket at from_idx
     cur = from_idx
@@ -28,56 +42,30 @@ def find_closing_bracket(tokens, from_idx):
                 return cur
         cur += 1
 
-def parse_from_to(tokens, from_idx, to_idx):
+def parse_from_to(tokens, from_idx, to_idx, part=1):
     # Replace contents of brackets with their flat value
     while '(' in tokens[from_idx:to_idx]:
         opening = tokens.index('(', from_idx, to_idx)
         closing = find_closing_bracket(tokens, opening)
-        middle = parse_from_to(tokens, opening + 1, closing)
+        middle = parse_from_to(tokens, opening + 1, closing, part)
         tokens = tokens[0:opening] + [str(middle)] + tokens[closing+1:to_idx]
         to_idx -= (closing - opening)
 
     # Calculate the flat value for this expression
-    return calc_flat_formula(tokens, from_idx, to_idx)
-
-def parse_subformula(tokens, cur_idx):
-    acc = 0
-    last_op = None
-    while cur_idx < len(tokens):
-        token = tokens[cur_idx]
-        next_idx = cur_idx + 1
-        if token == '(':
-            sub_result, end_idx = parse_subformula(tokens, cur_idx + 1)
-            next_idx = end_idx + 1
-        elif token in ('*', '+'):
-            last_op = token
-            cur_idx += 1
-            continue
-        elif token == ')':
-            return acc, cur_idx
-        else:
-            sub_result = int(token)
-
-        if last_op is None:
-             acc = sub_result
-        elif last_op == '+':
-            acc += sub_result
-        else:
-            acc *= sub_result
-        cur_idx = next_idx
-    return acc
+    if part == 1:
+        return accumulate_flat_formula(tokens, from_idx, to_idx)
+    else:
+        return calc_flat_formula(tokens, from_idx, to_idx)
         
-def read_formula(line, part_two=False):
+def read_formula(line, part=1):
     tokens = [c for c in re.sub(' ', '', line)]
-    if part_two:
-        return parse_from_to(tokens, 0, len(tokens))
-    return parse_subformula(tokens, 0)
+    return parse_from_to(tokens, 0, len(tokens), part)
 
 def part_one(line_gen):
     return sum(read_formula(inp) for inp in line_gen)
 
 def part_two(line_gen):
-    return sum(read_formula(inp, part_two=True) for inp in line_gen)
+    return sum(read_formula(inp, part=2) for inp in line_gen)
 
 print("A: " + str(part_one(lib.get_input(18))))
 print("B: " + str(part_two(lib.get_input(18))))
